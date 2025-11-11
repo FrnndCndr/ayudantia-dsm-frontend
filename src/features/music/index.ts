4c4a5775f5a35e5315e299c7cb59a6090b6224f3
frnnd <fdcg.contacto1@gmail.com>
2025-11-11 20:42:35 -0300

    feat: agrega archivo de índice para exportar componentes y hooks de música

---
+++ src/features/music/index.ts
@@ -0,0 +1,5 @@
+export { default as NowPlayingScreen } from './screens/NowPlayingScreen';
+export * from './models/song.models';
+export * from './api/musicService';
+export * from './hooks/useSongs';
+export * from './hooks/useAudioPlayer';
